f61826b49f2a8caca02d601b9526fe6f49c119c8
kunalmahajan121 <139353763+kunalmahajan121@users.noreply.github.com>
2023-09-21 09:58:36 +0000

    day02_commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const account_id = 14453;
+let account_email = "kunal@gmail.com"
+var account_pass = "123456"
+account_city = "Indore"
+let account_state;
+
+console.table([account_id,account_email,account_pass,account_city,account_state]);
